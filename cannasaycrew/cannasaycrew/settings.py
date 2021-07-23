e181500ec968aeaebfdda6d47456e41105f08fa2
nnnoc <vikchehovich@gmail.com>
2021-07-23 15:51:57 +0300

    слить все пароли

--- cannasaycrew/cannasaycrew/settings.py
+++ cannasaycrew/cannasaycrew/settings.py
@@ -125,3 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mediandrey@gmail.com'
+EMAIL_HOST_PASSWORD = '2506200aN'
